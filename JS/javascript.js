fb659a9cd0ee8e47f6d6f50ad9760917f7514ee0
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-24 23:53:52 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -6,8 +6,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
-async function addDistrictsGeoJson((geojsons\tartu_city_districts_edu.geojson) {
- const response = await fetch((geojsons\tartu_city_districts_edu.geojson)
+async function addDistrictsGeoJson(geojsons\tartu_city_districts_edu.geojson) {
+ const response = await fetch(geojsons\tartu_city_districts_edu.geojson)
  const data = await response.json()
  const polygons = L.geoJson(data)
  L.geoJson(data).addTo(map)
