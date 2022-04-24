19145376114fe2ce00e6944985adc6b835429248
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-24 23:45:53 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,16 +1,14 @@
 var map = L.map('map').setView([58.373523, 26.716045], 12);
-
+async function addDistrictsGeoJson() {
+ const response = await fetch()
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ L.geoJson(data).addTo(map)
+}
+addDistrictsGeoJson()
 const osm =
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  maxZoom: 19,
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
-
-async function addDGeoJson() {
- const response = await fetch(C:\Users\ragae\WebMapping\Lab2_WebProject\geojsons\tartu_city_districts_edu.geojson)
- const data = await response.json()
- const polygons = L.geoJson(data)
- L.geoJson(data).addTo(map);
-}
-addGeoJson();
