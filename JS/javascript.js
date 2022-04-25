d143f39142e6eaf6ffdab07eca1b0ca1e5d1c862
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-25 12:46:17 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -6,15 +6,3 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
-function popUPinfo(feature, layer) {
- layer.bindPopup(feature.properties.NIMI)
-}
-async function addDistrictsGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const polygons = L.geoJson(data)
-  onEachFeature: popUPinfo,
- })
- polygons.addTo(map)
-}
-addDistrictsGeoJson('geojsons/tartu_city_districts_edu.geojson')
