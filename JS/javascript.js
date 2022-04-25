7ddc7d0aca23530b68c763ec78a26f4787b9befb
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-25 12:25:51 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([58.373523, 26.716045], 12);
+let map = L.map('map').setView([58.373523, 26.716045], 12)
 
 const osm =
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
