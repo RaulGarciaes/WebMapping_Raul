59f5d6903b37dbc7438131e5f4f6e0ec404e8d0d
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-26 15:10:11 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm =
-      
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+
+const osm =     
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  maxZoom: 19,
  attribution: 'OpenStreetMap contributors',
