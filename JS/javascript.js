5a35c0ba8406222088e57f75e6fd01a49959ab95
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-26 13:27:09 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -6,3 +6,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data, {
+ onEachFeature: popUPinfo,
+ })
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojsons/tartu_city_districts_edu.geojson')
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ markers.addTo(map)
+}
+addCelltowersGeoJson('geojsons/tartu_city_celltowers_edu.geojson')
+function defaultMapSettings() {
+ map.setView([58.373523, 26.716045], 12)
+}
