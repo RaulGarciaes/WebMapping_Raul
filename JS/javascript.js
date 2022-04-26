a086ed57da0d8d33b42ea3209e6af6e39d3ee60d
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-26 14:25:08 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,11 +1,9 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
-
-const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: 'OpenStreetMap contributors',
-})
-osm.addTo(map)
+const Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
+Stadia_AlidadeSmoothDark.addTo(map)
 function popUPinfo(feature, layer) {
  layer.bindPopup(feature.properties.NIMI)
 }
