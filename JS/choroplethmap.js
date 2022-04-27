49e292fe01061ac8e37ed01dffd817cfc6aae1d4
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-27 11:22:57 +0300

    Update choroplethmap.js

--- JS/choroplethmap.js
+++ JS/choroplethmap.js
@@ -1,1 +1,36 @@
-m
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+
+const osm =     
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+})
+osm.addTo(map)
+
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
+
+addDistrictsGeoJson('geojsons/tartu_city_districts_edu.geojson')
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data, {
+  onEachFeature: popUPinfo,
+ })
+ polygons.addTo(map)
+}
+
+addCelltowersGeoJson('geojsons/tartu_city_celltowers_edu.geojson')
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ const clusters = L.markerClusterGroup()
+ clusters.addLayer(markers)
+ clusters.addTo(map) 
+}
+
+function defaultMapSettings() {
+ map.setView([58.373523, 26.716045], 12)
+}
