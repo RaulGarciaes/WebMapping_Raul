6829674ee8fbd2785a14244c99223eadbf941342
RaulGarciaes <104060956+RaulGarciaes@users.noreply.github.com>
2022-04-27 10:56:42 +0300

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -15,7 +15,7 @@ addDistrictsGeoJson('geojsons/tartu_city_districts_edu.geojson')
 async function addDistrictsGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
- const polygons = L.geoJson(data){
+ const polygons = L.geoJson(data, {
   onEachFeature: popUPinfo,
  })
  polygons.addTo(map)
